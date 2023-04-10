2c58703dd6bea0889467e51a8f69f5a5eb62a1c5
Djmalevsky <djmalevsky@gmail.com>
2023-04-10 19:49:38 -0400

    Initial commit

---
+++ resources/js/Pages/Inertia.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Inertia Page</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'This is a simple Inertia page',
+    };
+  },
+};
+</script>
